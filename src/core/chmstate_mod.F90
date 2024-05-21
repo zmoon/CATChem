90fcf50578021239e3e48419090554d59f698316
Barry Baker <barry.baker@noaa.gov>
2024-05-21 13:51:05 -0400

    pre-commit

--- src/core/chmstate_mod.F90
+++ src/core/chmstate_mod.F90
@@ -1,54 +1,54 @@
 module ChmState_Mod
-    !
-    ! USES:
-    !
-    USE Error_Mod
-    USE Precision_Mod
+   !
+   ! USES:
+   !
+   USE Error_Mod
+   USE Precision_Mod
 
-    IMPLICIT NONE
-    PRIVATE
-    !
-    ! !PUBLIC MEMBER FUNCTIONS:
-    PUBLIC :: Chm_Allocate
-    !
-    ! !Private DATA MEMBERS:
-    !
-    !=========================================================================
-    !
+   IMPLICIT NONE
+   PRIVATE
+   !
+   ! !PUBLIC MEMBER FUNCTIONS:
+   PUBLIC :: Chm_Allocate
+   !
+   ! !Private DATA MEMBERS:
+   !
+   !=========================================================================
+   !
 
-    type, public :: ChmStateType
-        !---------------------------------------------------------------------
-        ! Name of variables containing chemistry information
-        !---------------------------------------------------------------------
-        CHARACTER(LEN=4)  :: State     = 'Chem'    ! Name of this state
+   type, public :: ChmStateType
+      !---------------------------------------------------------------------
+      ! Name of variables containing chemistry information
+      !---------------------------------------------------------------------
+      CHARACTER(LEN=4)  :: State     = 'Chem'    ! Name of this state
 
-        !---------------------------------------------------------------------
-        ! Integers
-        !---------------------------------------------------------------------
-        INTEGER           :: nSpecies
-        INTEGER           :: nSpeciesGas
-        INTEGER           :: nSpeciesAero
-        INTEGER           :: nSpeciesDust
-        INTEGER           :: NSpeicesSeaSalt
-        INTEGER, POINTER  :: AeroIndex(:)
-        INTEGER, POINTER  :: GasIndex(:)
-        INTEGER, POINTER  :: DustIndex(:)
-        INTEGER, POINTER  :: SeaSaltIndex(:)
+      !---------------------------------------------------------------------
+      ! Integers
+      !---------------------------------------------------------------------
+      INTEGER           :: nSpecies
+      INTEGER           :: nSpeciesGas
+      INTEGER           :: nSpeciesAero
+      INTEGER           :: nSpeciesDust
+      INTEGER           :: NSpeicesSeaSalt
+      INTEGER, POINTER  :: AeroIndex(:)
+      INTEGER, POINTER  :: GasIndex(:)
+      INTEGER, POINTER  :: DustIndex(:)
+      INTEGER, POINTER  :: SeaSaltIndex(:)
 
-        !---------------------------------------------------------------------
-        ! Reals
-        !---------------------------------------------------------------------
-        REAL(fp), POINTER :: ChmSpecies(:,:,:,:)
+      !---------------------------------------------------------------------
+      ! Reals
+      !---------------------------------------------------------------------
+      REAL(fp), POINTER :: ChmSpecies(:,:,:,:)
 
-    end type ChmStateType
+   end type ChmStateType
 
-    CONTAINS
-    !
-    !=========================================================================
-    !
-    subroutine Chm_Allocate(Config_Opt, State_Grid, State_Chm, RC)
+CONTAINS
+   !
+   !=========================================================================
+   !
+   subroutine Chm_Allocate(Config_Opt, State_Grid, State_Chm, RC)
 
-        ! USES
-        USE Config_Opt_Mod, ONLY : OptConfig
-        USE GridState_Mod,  ONLY : GridStateType
-        USE ChmState_Mod,   ONLY : ChmStateType
+      ! USES
+      USE Config_Opt_Mod, ONLY : OptConfig
+      USE GridState_Mod,  ONLY : GridStateType
+      USE ChmState_Mod,   ONLY : ChmStateType
--- src/core/diagstate_mod.F90
+++ src/core/diagstate_mod.F90
@@ -1,70 +1,70 @@
 module DiagState_Mod
-    ! Uses
-    USE Precision_Mod
-    USE Error_Mod
-    
-    IMPLICIT NONE
-    private
+   ! Uses
+   USE Precision_Mod
+   USE Error_Mod
 
-    ! PUBLIC :: Zero_State_Diag
-    PUBLIC :: Diag_Allocate
+   IMPLICIT NONE
+   private
 
-    type, public :: DiagStateType
+   ! PUBLIC :: Zero_State_Diag
+   PUBLIC :: Diag_Allocate
 
-        ! Reals
-        REAL(fp), POINTER :: dust_total_flux(:,:)
-        REAL(fp), POINTER :: sea_salt_total_flux(:,:)
+   type, public :: DiagStateType
 
-    end type DiagStateType
+      ! Reals
+      REAL(fp), POINTER :: dust_total_flux(:,:)
+      REAL(fp), POINTER :: sea_salt_total_flux(:,:)
 
-    CONTAINS
+   end type DiagStateType
 
-    subroutine Diag_Allocate(Config_Opt, State_Grid, State_Diag, RC)
-        ! USES
-        USE GridState_Mod, ONLY : GridStateType
-        USE Config_Opt_Mod, ONLY : OptConfig
+CONTAINS
 
-        IMPLICIT NONE
+   subroutine Diag_Allocate(Config_Opt, State_Grid, State_Diag, RC)
+      ! USES
+      USE GridState_Mod, ONLY : GridStateType
+      USE Config_Opt_Mod, ONLY : OptConfig
 
-        ! INOUT Params
-        type(OptConfig),     INTENT(in)    :: Config_Opt ! Input Options object
-        type(GridStateType), INTENT(in)    :: State_Grid ! Grid State object
-        type(DiagStateType), INTENT(inout) :: State_Diag ! Diag State object
-        ! OUTPUT Params
-        INTEGER,             INTENT(OUT)   :: RC          ! Success or failure
+      IMPLICIT NONE
 
-        ! Error handling
-        CHARACTER(LEN=255) :: ErrMsg
-        CHARACTER(LEN=255) :: thisLoc
+      ! INOUT Params
+      type(OptConfig),     INTENT(in)    :: Config_Opt ! Input Options object
+      type(GridStateType), INTENT(in)    :: State_Grid ! Grid State object
+      type(DiagStateType), INTENT(inout) :: State_Diag ! Diag State object
+      ! OUTPUT Params
+      INTEGER,             INTENT(OUT)   :: RC          ! Success or failure
 
-        ! Initialize
-        RC = 0
-        ErrMsg = ''
-        thisLoc = ' -> at Diag_Allocate (in core/diagstate_mod.F90)'
+      ! Error handling
+      CHARACTER(LEN=255) :: ErrMsg
+      CHARACTER(LEN=255) :: thisLoc
 
-        ! Nullify all fields for safety's sake before allocating them
-        ! This can prevent compilation errors caused by uninitialized values
-        ! Nullify all fields for safety's sake before allocating them
-        ! This can prevent compilation errors caused by uninitialized values
-        State_Diag%dust_total_flux => NULL()
-        State_Diag%sea_salt_total_flux => NULL()
+      ! Initialize
+      RC = 0
+      ErrMsg = ''
+      thisLoc = ' -> at Diag_Allocate (in core/diagstate_mod.F90)'
 
-        ! If dust process is activated then allocate dust related diagnostics
-        if (Config_Opt%dust_activate) then
-            ALLOCATE( State_Diag%dust_total_flux( State_Grid%NX, State_Grid%NY ), STAT=RC )
-            CALL CC_CheckVar( 'State_Diag%dust_total_flux', 0, RC )
-            IF ( RC /= CC_SUCCESS ) RETURN
-            State_Diag%dust_total_flux = 0e+0_fp
-        endif
+      ! Nullify all fields for safety's sake before allocating them
+      ! This can prevent compilation errors caused by uninitialized values
+      ! Nullify all fields for safety's sake before allocating them
+      ! This can prevent compilation errors caused by uninitialized values
+      State_Diag%dust_total_flux => NULL()
+      State_Diag%sea_salt_total_flux => NULL()
 
-        ! If sea salt process is activated then allocate sea salt related diagnostics
-        if (Config_Opt%seasalt_activate) then
-            ALLOCATE( State_Diag%sea_salt_total_flux( State_Grid%NX, State_Grid%NY ), STAT=RC )
-            CALL CC_CheckVar( 'State_Diag%sea_salt_total_flux', 0, RC )
-            IF ( RC /= CC_SUCCESS ) RETURN
-            State_Diag%sea_salt_total_flux = 0e+0_fp
-        endif
+      ! If dust process is activated then allocate dust related diagnostics
+      if (Config_Opt%dust_activate) then
+         ALLOCATE( State_Diag%dust_total_flux( State_Grid%NX, State_Grid%NY ), STAT=RC )
+         CALL CC_CheckVar( 'State_Diag%dust_total_flux', 0, RC )
+         IF ( RC /= CC_SUCCESS ) RETURN
+         State_Diag%dust_total_flux = 0e+0_fp
+      endif
 
-    end subroutine Diag_Allocate
+      ! If sea salt process is activated then allocate sea salt related diagnostics
+      if (Config_Opt%seasalt_activate) then
+         ALLOCATE( State_Diag%sea_salt_total_flux( State_Grid%NX, State_Grid%NY ), STAT=RC )
+         CALL CC_CheckVar( 'State_Diag%sea_salt_total_flux', 0, RC )
+         IF ( RC /= CC_SUCCESS ) RETURN
+         State_Diag%sea_salt_total_flux = 0e+0_fp
+      endif
+
+   end subroutine Diag_Allocate
 
 end module DiagState_Mod
