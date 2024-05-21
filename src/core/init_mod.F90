2646768b5653ab5a586600625a6d2c167e7756b6
Barry Baker <barry.baker@noaa.gov>
2024-05-21 10:42:47 -0400

    rename input_mod and input_opt_mod

--- src/core/init_mod.F90
+++ src/core/init_mod.F90
@@ -3,13 +3,55 @@ module init_mod
    implicit none
 
    PUBLIC :: base_config_yaml_read
+   PUBLIC :: Init_Met
 
 contains
 
-   subroutine base_config_yaml_read( RC)
+   subroutine Init_Met(State_Grid, State_Met, RC)
+      use GridState_Mod, Only : GrdState
+      use MetState_Mod
+      USE Error_Mod
+      implicit none
+
+      ! Arguments
+      TYPE(GrdState), INTENT(IN)  :: State_Grid
+      TYPE(MetState), INTENT(INOUT) :: State_Met
+      INTEGER,        INTENT(OUT) :: RC
+
+      ! Local variables
+      CHARACTER(LEN=255) :: ErrMsg, thisLoc
+
+      ! Initialize
+      RC = 0
+      ErrMsg = ''
+      thisLoc = ' -> at Init_Met (in core/state_mod.F90)'
+
+      call Zero_State_Met(State_Met, RC)
+      if (RC /= CC_SUCCESS) then
+         errMsg = 'Error Nullifying met state'
+         call CC_Error( errMsg, RC , thisLoc)
+      endif
+
+      call Met_Allocate(State_Grid, State_Met, RC)
+      if (RC /= CC_SUCCESS) then
+         errMsg = 'Error allocating met state'
+         call CC_Error( errMsg, RC , thisLoc)
+      endif
+   end subroutine Init_Met 
+
+   subroutine base_config_yaml_read(Config_Opt, State_Grid, RC)
       use QfYaml_Mod
       use Error_Mod
+      use Config_Opt_Mod
+      use GridState_Mod, Only : GrdState
+
       implicit none
+
+      ! INOUT Params
+      type(OptConfig), intent(inout) :: Config_Opt ! Input Options object
+      type(OptConfig), intent(inout) :: State_Grid ! Input Options object
+      ! OUTPUT Params
+      INTEGER,         INTENT(OUT)   :: RC          ! Success or failure
 
       ! Local Params
       !-------------
@@ -17,18 +59,15 @@ contains
       ! Characters
       CHARACTER(LEN=18), PARAMETER :: configFile ='CATChem_config.yml'
       CHARACTER(LEN=512) :: errMsg
-      CHARACTER(LEN=512) :: thisLoc
+      CHARACTER(LEN=255) :: thisLoc
 
       ! QFYAML_t type
       TYPE(QFYAML_t)     :: Config, ConfigAnchored
 
-      ! Integers
-      INTEGER :: RC          ! Success or failure
-
       ! set thisLoc
       thisLoc = 'init_mod::base_config_yaml_read() -> at read CATChem_Conifg.yml'
       errMsg = ''
-      RC = CC_SUCCESS
+      RC = 0
 
       WRITE( 6, '(a  )' ) REPEAT( '=', 79 )
       WRITE( 6, 100   ) TRIM( configFile )
--- tests/test_main.F90
+++ tests/test_main.F90
@@ -3,15 +3,64 @@
 
 program test_main
    USE init_mod
+   USE Error_Mod
+   Use State_Mod
+   USE Config_Mod
+
    IMPLICIT NONE
 
    ! Integers
    INTEGER:: RC          ! Success or failure
 
-   call base_config_yaml_read(RC)
+   ! Error handling
+   CHARACTER(LEN=512) :: errMsg
+   CHARACTER(LEN=255) :: thisLoc
+   CHARACTER(LEN=18), PARAMETER :: configFile ='CATChem_config.yml'
+   ! set thisLoc
+   thisLoc = 'test_main::test_main() -> at read CATChem_Conifg.yml'
+   errMsg = ''
+   RC = 0
 
-   if (RC /= 0) then
-      stop 1
-   end if
+   write(*,*) '   CCCCC      A     TTTTTTT   CCCCC  H'
+   write(*,*) '  C          A A       T     C       H       CCCC    EEEE  M       M'
+   write(*,*) '  C         AAAAA      T     C       HHHHH  C      E    E  M M   M M'
+   write(*,*) '  C        A     A     T     C       H   H  C      E EE    M   M   M'    
+   write(*,*) '   CCCCC  A       A    T      CCCCC  H   H   CCCC   EEEEE  M       M' 
+   write(*,*) ''
+   write(*,*) ''
+
+   ! Read input file and initialize grid
+   call Read_Input_File(Config_Opt, GRID, RC)
+   if (RC /= CC_success) then
+      errMsg = 'Error reading configuration file: ' // TRIM( configFile )
+      call CC_Error( errMsg, RC , thisLoc)
+   endif
+
+   ! write grid info
+   write(*,*) 'Grid info:'
+   write(*,*) 'Number of grid nx = ', GRID%NX
+   write(*,*) 'Number of grid ny = ', GRID%NY
+   write(*,*) 'Number of grid levels = ', GRID%number_of_levels
+   
+   ! initialize met
+   call Init_Met(GRID, MET, RC)
+   if (RC /= CC_success) then
+      errMsg = 'Error initializing meteorology'
+      call CC_Error( errMsg, RC , thisLoc)
+   endif
+
+   write(*,*) 'Finished CATChem'
+   ! call base_config_yaml_read(RC)
+
+   ! read yaml file
+   ! if (RC /= CC_success) then
+   !    errMsg = 'Error reading configuration file: ' // TRIM( configFile )
+   !    call CC_Error( errMsg, RC , thisLoc)
+   !    return
+   ! endif
+
+   ! allocate arrays for current state
+   ! call Init_State(Config_Opt, State_Grid, State_Met, RC)
+
 
 end program test_main
