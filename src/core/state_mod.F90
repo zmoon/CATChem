6fa40217c770478dd730a1bed692b771b6e91e86
Barry Baker <barry.baker@noaa.gov>
2024-06-28 16:17:27 -0400

    use state_mod instead of importing everything individually

--- src/core/state_mod.F90
+++ src/core/state_mod.F90
@@ -9,9 +9,11 @@
 module state_mod
    use precision_mod
    use Config_Opt_Mod, only : ConfigType
-   use GridState_Mod, only : GridStateType
-   use MetState_Mod, only : MetStateType
-   use ChemState_Mod, only : ChemStateType
+   use GridState_Mod,  only : GridStateType
+   use MetState_Mod,   only : MetStateType
+   use ChemState_Mod,  only : ChemStateType
+   use EmisState_Mod,  only : EmisStateType
+   use DiagState_Mod,  only : DiagStateType
 
    IMPLICIT NONE
 
@@ -19,6 +21,8 @@ module state_mod
    type(GridStateType), PUBLIC :: GridState
    type(MetStateType),  PUBLIC :: MetState
    type(ChemStateType), PUBLIC :: ChemState
-   type(ConfigType),    PUBLIC  :: Config
+   type(ConfigType),    PUBLIC :: Config
+   type(EmisStateType), PUBLIC :: EmisState
+   type(DiagStateType), PUBLIC :: DiagState
 
 end module state_mod
