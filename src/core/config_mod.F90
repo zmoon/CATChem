1f15992ad95492227b3587513f3954d86d39ce38
zmoon <zmoon92@gmail.com>
2024-07-23 14:16:15 -0500

    Revert "add megan options to core of the model"
    This reverts commit 8662b4e126ae1375c81aee90bc3ca8e12288a9eb.

--- src/core/config_mod.F90
+++ src/core/config_mod.F90
@@ -143,7 +143,7 @@ CONTAINS
          RETURN
       ENDIF
 
-      call Config_Chem_State(config%Species_File, GridState,ChemState, RC)
+      call Config_Chem_State(config%Species_File, GridState, ChemState, RC)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Error in "Config_Chem_State"!'
          CALL CC_Error( errMsg, RC, thisLoc  )
@@ -153,6 +153,13 @@ CONTAINS
       endif
 
       call Config_Emis_State(config%Emission_File, EmisState, ChemState, RC)
+      if (RC /= CC_SUCCESS) then
+         errMsg = 'Error in "Config_Emis_State"!'
+         CALL CC_Error( errMsg, RC, thisLoc  )
+         CALL QFYAML_CleanUp( ConfigInput )
+         CALL QFYAML_CleanUp( ConfigAnchored )
+         RETURN
+      endif
 
       !========================================================================
       ! Further error-checking and initialization
