d795513bb35a183d969b35ff9bc522c4681990b5
Barry Baker <barry.baker@noaa.gov>
2024-07-30 09:30:11 -0400

    create test function for plumerise

--- tests/test_plumerise.F90
+++ tests/test_plumerise.F90
@@ -1,94 +1,145 @@
 program test_plumerise
    use CATChem, fp => cc_rk
-   use testing_mod, only: assert
+   use testing_mod, only: assert, load_column_data
    use state_mod
+
 
    implicit none
 
-   integer, parameter :: u = 99  !< Unit number for loading column data
+   type(PlumeriseStateType) :: PlumeriseState
 
-   integer :: i
-   integer :: ios  !< File I/O status
-   integer :: n  !< Size of data to load
-   character(len=255) :: vn  !< Variable name
+   integer :: rc          ! Success or failure
+   integer :: c, s, l
 
-   i = 0
-   vn = ""
-   n = 0
+   CHARACTER(LEN=255), PARAMETER :: configFile ='Configs/Plumerise/CATChem_config.yml'
 
-   ! Load the data
-   open(unit=u, file="col.csv", iostat=ios)
-   if (ios /= 0) error stop
-   do
-      if (mod(i, 2) == 0) then
-         ! Variable info
-         read(u, *, iostat=ios) vn, n
-         if (ios /= 0) exit
-      else
-         ! Data
-         print *, "Reading: " // trim(vn)
-         select case (vn)
-          case ("hflux")
-            read(u, *, iostat=ios) MetState%HFLUX
-            if (ios /= 0) exit
-          case ("pblh")
-            read(u, *, iostat=ios) MetState%PBLH
-            if (ios /= 0) exit
-          case ("pmid")
-            allocate(MetState%PMID(n))
-            read(u, *, iostat=ios) MetState%PMID
-            if (ios /= 0) exit
-          case ("ps")
-            read(u, *, iostat=ios) MetState%PS
-            if (ios /= 0) exit
-          case ("qv")
-            allocate(MetState%QV(n))
-            read(u, *, iostat=ios) MetState%QV
-            if (ios /= 0) exit
-          case ("t")
-            allocate(MetState%T(n))
-            read(u, *, iostat=ios) MetState%T
-            if (ios /= 0) exit
-          case ("t2m")
-            read(u, *, iostat=ios) MetState%T2M
-            if (ios /= 0) exit
-          case ("u")
-            allocate(MetState%U(n))
-            read(u, *, iostat=ios) MetState%U
-            if (ios /= 0) exit
-          case ("ustar")
-            read(u, *, iostat=ios) MetState%USTAR
-            if (ios /= 0) exit
-          case ("v")
-            allocate(MetState%V(n))
-            read(u, *, iostat=ios) MetState%V
-            if (ios /= 0) exit
-          case ("z")
-            allocate(MetState%Z(n))
-            read(u, *, iostat=ios) MetState%Z
-            if (ios /= 0) exit
-          case ("zmid")
-            allocate(MetState%ZMID(n))
-            read(u, *, iostat=ios) MetState%ZMID
-            if (ios /= 0) exit
-          case default
-            print *, "Unknown variable name: " // trim(vn)
-            error stop
-         end select
-      end if
-      i = i + 1
-   end do
-   print *, "HFLUX:", MetState%HFLUX
-   print *, "PBLH:", MetState%PBLH
-   print *, "PMID:", MetState%PMID
-   print *, "PS:", MetState%PS
-   print *, "QV:", MetState%QV
-   print *, "T:", MetState%T
-   print *, "T2M:", MetState%T2M
-   print *, "U:", MetState%U
-   print *, "USTAR:", MetState%USTAR
-   print *, "V:", MetState%V
-   print *, "Z:", MetState%Z
-   print *, "ZMID:", MetState%ZMID
+   ! Error handling
+   CHARACTER(LEN=255) :: errMsg
+   CHARACTER(LEN=255) :: thisLoc
+
+   real, ALLOCATABLE :: PMID(:)
+   real, ALLOCATABLE :: QV(:)
+   real, ALLOCATABLE :: T(:)
+   real, ALLOCATABLE :: U(:)
+   real, ALLOCATABLE :: V(:)
+   real, ALLOCATABLE :: Z(:)
+   real, ALLOCATABLE :: ZMID(:)
+
+   thisLoc = 'test_plumerise -> at read CATChem_Config.yml'
+   errMsg = ''
+   rc = CC_SUCCESS
+
+   write(*,*) '   CCCCC      A     TTTTTTT   CCCCC  H'
+   write(*,*) '  C          A A       T     C       H       CCCC   EEEE   M       M'
+   write(*,*) '  C         AAAAA      T     C       HHHHH  C      E    E  M M   M M'
+   write(*,*) '  C        A     A     T     C       H   H  C      E EE    M   M   M'
+   write(*,*) '   CCCCC  A       A    T      CCCCC  H   H   CCCC   EEEEE  M       M'
+   write(*,*) ''
+   write(*,*) '  PLUMERISE TEST'
+
+   ! Read input file and initialize grid
+   call cc_read_config(Config, GridState, EmisState, ChemState, rc, configFile)
+   if (rc /= CC_success) then
+      errMsg = 'Error reading configuration file: ' // TRIM( configFile )
+      call cc_emit_error(errMsg, rc, thisLoc)
+      stop 1
+   endif
+
+   ! Allocate MetState
+   MetState%nSOIL = 4
+   print*, 'Allocating MetState'
+   call cc_allocate_metstate(GridState, MetState, rc)
+   if (rc /= CC_success) then
+      errMsg = 'Error in "cc_allocate_metstate"'
+      call cc_emit_error(errMsg, rc, thisLoc)
+      stop 1
+   endif
+   print*, 'Allocated MetState'
+
+   ! Meteorological State
+   call load_column_data("MetProfiles/Profile_NCWCP.csv", MetState, rc)
+
+   ! Allocate EmisState with FRP
+   do c = 1, EmisState%nCats
+      if (EmisState%Cats(c)%nPlumerise /= 0) then
+         do s = 1, EmisState%Cats(c)%nSpecies
+            if (EmisState%Cats(c)%Species(s)%plumerise == 1) then
+
+               EmisState%Cats(c)%Species(s)%nPlmSrc = 4
+               allocate(EmisState%Cats(c)%Species(s)%PlmSrcFlx(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               allocate(EmisState%Cats(c)%Species(s)%FRP(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               EmisState%Cats(c)%Species(s)%FRP = (/10.0e6, 100.0e6, 500.0e6, 1000.0e6/)
+               EmisState%Cats(c)%Species(s)%PlmSrcFlx = 0.0
+
+            else if (EmisState%Cats(c)%Species(s)%plumerise == 2) then
+
+               EmisState%Cats(c)%Species(s)%nPlmSrc = 1
+               allocate(EmisState%Cats(c)%Species(s)%PlmSrcFlx(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               allocate(EmisState%Cats(c)%Species(s)%STKDM(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               allocate(EmisState%Cats(c)%Species(s)%STKHT(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               allocate(EmisState%Cats(c)%Species(s)%STKTK(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               allocate(EmisState%Cats(c)%Species(s)%STKVE(EmisState%Cats(c)%Species(s)%nPlmSrc), stat=rc)
+               if (rc /= CC_success) then
+                  errMsg = 'Error in "cc_allocate_emisstate"'
+                  call cc_emit_error(errMsg, rc, thisLoc)
+                  stop 1
+               endif
+               EmisState%Cats(c)%Species(s)%STKDM = (/30.0/)
+               EmisState%Cats(c)%Species(s)%STKHT = (/10.0/)
+               EmisState%Cats(c)%Species(s)%STKTK = (/273.0/)
+               EmisState%Cats(c)%Species(s)%STKVE = (/10.0/)
+
+            endif
+         enddo
+      endif
+   enddo
+
+   ! Plumerise init
+   call cc_plumerise_init(PlumeriseState, rc)
+   if (rc /= CC_success) then
+      errMsg = 'Error in "cc_plumerise_init"'
+      call cc_emit_error(errMsg, rc, thisLoc)
+      stop 1
+   endif
+
+   ! Plumerise run
+   call cc_plumerise_run(PlumeriseState, rc, verbose_opt=.true.)
+   if (rc /= CC_success) then
+      errMsg = 'Error in "cc_plumerise_run"'
+      call cc_emit_error(errMsg, rc, thisLoc)
+      stop 1
+   endif
+
+
 
 end program test_plumerise
