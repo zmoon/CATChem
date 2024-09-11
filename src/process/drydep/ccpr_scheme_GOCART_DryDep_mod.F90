9513ff05406cce812124fed96817e852b45af3d6
bbakernoaa <barry.baker@noaa.gov>
2024-09-11 14:21:24 +0000

    BB rework

--- src/process/drydep/ccpr_scheme_GOCART_DryDep_mod.F90
+++ src/process/drydep/ccpr_scheme_GOCART_DryDep_mod.F90
@@ -31,51 +31,126 @@ contains
    !!
    !! Note that other state types may be required, e.g. one specific to the process group.
    !!!>
-   subroutine CCPr_Scheme_GOCART_DryDep(MetState, DiagState, RC)
+   subroutine CCPr_Scheme_GOCART_DryDep(km,
+                                        tmpu,
+                                        rhoa,
+                                        hghte,
+                                        lwi,
+                                        ustar,
+                                        pblh,
+                                        hflux,
+                                        von_karman,
+                                        cp,
+                                        g0,
+                                        radius,
+                                        rhop,
+                                        u10,
+                                        v10,
+                                        fraclake,
+                                        gwettop,
+                                        ResuspentionOpt,
+                                        drydepf,
+                                        RC,
+                                        )
 
       ! Uses
-      USE Constants,     Only : g0, von_karman, cp
-      use precision_mod, only : fp, ZERO
-      Use MetState_Mod,  Only : MetStateType  ! Needed to access Meteorological Variables
-      Use DiagState_Mod, Only : DiagStateType ! Diagnostic Variables are added through DiagState below
-      Use Error_Mod,     Only : CC_SUCCESS    ! Error Check Success
-      Use CCPr_DryDep_mod                     ! Overall DryDep State Type - Controlling DryDep
+      USE GOCART2G, only: DryDeposition
 
       IMPLICIT NONE
 
       ! Arguments
-      type(MetStateType),  intent(in) :: MetState     ! Meteorological Variables
-      type(DiagStateType), intent(in) :: DiagState    ! Diagnostic Variables
-      !type(DryDepStateType), intent(inout) :: DryDepState  ! DryDep Variables
-
+      INTEGER, intent(in)                     :: km     ! number of vertical levels 
+      INTEGER, pointer, intent(in)                      :: lwi                                    ! orography flag; Land, ocean, ice mask
+                     ! Deposition frequency [1/sec]
+      REAL, pointer, intent(in), DIMENSION(:) :: tmpu   ! Temperature [K]
+      REAL, pointer, intent(in), DIMENSION(:) :: rhoa   ! Air density [kg/m^3]
+      REAL, pointer, intent(in), DIMENSION(:) :: hghte  ! Height [m]
+      REAL, pointer, intent(in)                      :: radius                                ! particle radius [m]
+      REAL, pointer, intent(in)                      :: rhop                                  ! particle density [kg/m^3]
+      REAL, pointer, intent(in)                      :: ustar                                 ! friction speed [m/sec]
+      REAL, pointer, intent(in)                      :: pblh                                  ! PBL height [m]
+      REAL, pointer, intent(in)                      :: hflux                                 ! sfc. sens. heat flux [W m-2]
+      REAL, pointer, intent(in)                      :: z0h                                   ! rough height, sens. heat [m]
+      REAL, pointer, intent(in) :: u10m                   ! 10-m u-wind component [m/sec]
+      REAL, pointer, intent(in) :: v10m                   ! 10-m v-wind component [m/sec]
+      REAL, pointer, intent(in) :: fraclake               ! fraction covered by water [1]
+      REAL, pointer, intent(in) :: gwettop                ! fraction soil moisture [1]
+      real, intent(in) :: radius 
+      real, intent(in) :: rhop
+      real, intent(in) :: cp
+      real, intent(in) :: von_karman
+      logical, intent(in) ::  ResuspentionOpt
+      ! Output
+      REAL, intent(out)  :: drydepf
       integer, intent(out) :: RC                      ! Success or Failure
 
       ! Local Variables
+      real, allocatable, dimension(:,:,:) :: GOCART_tmpu 
+      real, allocatable, dimension(:,:,:)  :: GOCART_rhoa
+      real :: GOCART_LWI
+      real, allocatable, dimension(:,:,:) :: GOCART_HGHTE
+      real :: GOCART_USTAR
+      real :: GOCART_PBLH
+      real :: GOCART_HFLUX
+      real :: GOCART_Z0H
+      real :: GOCART_U10
+      real :: GOCART_V10
+      real :: GOCART_FRACLAKE
+      real :: GOCART_GWETTOP
+      real :: GOCART_DRYDEPF
       character(len=256) :: errMsg
       character(len=256) :: thisLoc
-
-      !integer :: processInt   ! Add Local Scheme Specific Variables < Modify as needed >
-      !real(fp) :: processFlt  ! Add Local Scheme Specific Variables < Modify as needed >
 
       ! Initialize
       errMsg = ''
       thisLoc = ' -> at CCPr_Scheme_GOCART_DryDep (in CCPr_Scheme_GOCART_mod.F90)'
       RC = CC_SUCCESS
 
+      ! transform data for GOCART DryDeposition call 
+
+      call PrepMetVarsForGOCART(km,
+                                tmpu,
+                                rhoa,
+                                hghte,
+                                u10,
+                                v10,
+                                fraclake,
+                                gwettop,
+                                lwi,
+                                ustar,
+                                pblh,
+                                hflux,
+                                z0h,
+                                GOCART_tmpu,
+                                GOCART_RHOA,
+                                GOCART_HGHTE,
+                                GOCART_U10
+                                GOCART_V10,
+                                GOCART_FRACLAKE,
+                                GOCART_GWETTOP,
+                                GOCART_LWI,
+                                GOCART_USTAR,
+                                GOCART_PBLH,
+                                GOCART_HFLUX,
+                                GOCART_Z0H)
       !------------------
       ! Begin Scheme Code
       !------------------
 
-      ! Begin GOCART Code
-
-      ! GOCART Options comes in from DryDepState
-      ! Diagnostic Variables are added through DiagState below
-
-      ! ADD GOCART CODE HERE
+      if (ResuspentionOpt) then 
+         call DryDeposition(km, GOCART_TMPU, GOCART_RHOA, GOCART_HGHTE, GOCART_LWI, GOCART_USTAR, & 
+                            GOCART_PBLH, von_karman, cp, g0, GOCART_Z0H, GOCART_DRYDEPF, RC, &
+                            radius, rhop, GOCART_U10, GOCART_V10, GOCART_FRACLAKE, GOCART_GWETTOP)
+      else 
+         call DryDeposition(km, GOCART_TMPU, GOCART_RHOA, GOCART_HGHTE, GOCART_LWI, GOCART_USTAR, & 
+                            GOCART_PBLH, von_karman, cp, g0, GOCART_Z0H, GOCART_DRYDEPF, RC)
+      endif
+   
         
-
+      drydepf = GOCART_DRYDEPF(1,1)
 
       ! End GOCART Code
+
 
    end subroutine CCPr_Scheme_GOCART_DryDep
 
@@ -100,34 +175,68 @@ contains
  !!
  !! \ingroup core_modules
  !!!>
-subroutine PrepMetVarsForGOCART(metstate, tmpu, rhoa, hghte, lwi, ustar, pblh, hflux, &
-   z0h, u10m, v10m, fraclake, gwettop, rc)
-   use MetState_Mod, only: MetStateType
+subroutine PrepMetVarsForGOCART(km,
+                                tmpu,
+                                rhoa,
+                                hghte,
+                                u10,
+                                v10,
+                                fraclake,
+                                gwettop,
+                                lwi,
+                                ustar,
+                                pblh,
+                                hflux,
+                                z0h,
+                                GOCART_tmpu,
+                                GOCART_RHOA,
+                                GOCART_HGHTE,
+                                GOCART_U10
+                                GOCART_V10,
+                                GOCART_FRACLAKE,
+                                GOCART_GWETTOP,
+                                GOCART_LWI,
+                                GOCART_USTAR,
+                                GOCART_PBLH,
+                                GOCART_HFLUX,
+                                GOCART_Z0)
+   
 
    IMPLICIT NONE
 
    ! INPUTS
-   type(MetStateType), intent(in) :: MetState                   !< Meteorological state
+   INTEGER, intent(in)                     :: km     ! number of vertical levels 
+   INTEGER, pointer, intent(in)                      :: lwi                                    ! orography flag; Land, ocean, ice mask
+   REAL, pointer, intent(in), DIMENSION(:) :: tmpu   ! Temperature [K]
+   REAL, pointer, intent(in), DIMENSION(:) :: rhoa   ! Air density [kg/m^3]
+   REAL, pointer, intent(in), DIMENSION(:) :: hghte  ! Height [m]
+   REAL, pointer, intent(in)                      :: radius                                ! particle radius [m]
+   REAL, pointer, intent(in)                      :: rhop                                  ! particle density [kg/m^3]
+   REAL, pointer, intent(in)                      :: ustar                                 ! friction speed [m/sec]
+   REAL, pointer, intent(in)                      :: pblh                                  ! PBL height [m]
+   REAL, pointer, intent(in)                      :: hflux                                 ! sfc. sens. heat flux [W m-2]
+   REAL, pointer, intent(in)                      :: z0h                                   ! rough height, sens. heat [m]
+   REAL, pointer, intent(in) :: u10m                   ! 10-m u-wind component [m/sec]
+   REAL, pointer, intent(in) :: v10m                   ! 10-m v-wind component [m/sec]
+   REAL, pointer, intent(in) :: fraclake               ! fraction covered by water [1]
+   REAL, pointer, intent(in) :: gwettop                ! fraction soil moisture [1]
 
    ! INPUT/OUTPUTS
-   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: tmpu   !< temperature [K]
-   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: rhoa   !< air density [kg/m^3]
-   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: hghte  !< geometric height [m]
-   REAL, DIMENSION(1,1), intent(inout) :: u10m                  !< 10-m u-wind component [m/sec]
-   REAL, DIMENSION(1,1), intent(inout) :: v10m                  !< 10-m v-wind component [m/sec]
-   REAL, DIMENSION(1,1), intent(inout) :: fraclake              !< fraction covered by water [1]
-   REAL, DIMENSION(1,1), intent(inout) :: gwettop               !< fraction soil moisture [1]
-   INTEGER, intent(inout) :: lwi                                !< orography flag; Land, ocean, ice mask
-   REAL,    intent(inout) :: ustar                              !< friction speed [m/sec]
-   REAL,    intent(inout) :: pblh                               !< PBL height [m]
-   REAL,    intent(inout) :: hflux                             !< sfc. sens. heat flux [W m-2]
-   REAL,    intent(inout) :: z0h                                !< rough height, sens. heat [m]
+   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: GOCART_TMPU   !< temperature [K]
+   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: GOCART_RHOA   !< air density [kg/m^3]
+   REAL, intent(inout), allocatable, DIMENSION(:,:,:) :: GOCART_HGHTE  !< geometric height [m]
+   REAL, DIMENSION(1,1), intent(inout) :: GOCART_U10                  !< 10-m u-wind component [m/sec]
+   REAL, DIMENSION(1,1), intent(inout) :: GOCART_V10                  !< 10-m v-wind component [m/sec]
+   REAL, DIMENSION(1,1), intent(inout) :: GOCART_FRACLAKE              !< fraction covered by water [1]
+   REAL, DIMENSION(1,1), intent(inout) :: GOCART_GWETTOP               !< fraction soil moisture [1]
+   INTEGER, intent(inout) :: GOCART_LWI                                !< orography flag; Land, ocean, ice mask
+   REAL,    intent(inout) :: GOCART_USTAR                              !< friction speed [m/sec]
+   REAL,    intent(inout) :: GOCART_PBLH                               !< PBL height [m]
+   REAL,    intent(inout) :: GOCART_HFLUX                              !< sfc. sens. heat flux [W m-2]
+   REAL,    intent(inout) :: GOCART_Z0H                                !< rough height, sens. heat [m]
 
    ! OUTPUTS
    INTEGER :: rc !< Return code
-
-   ! LOCAL VARIABLES
-   INTEGER :: km
 
    ! Error handling
    character(len=255) :: errMsg
@@ -138,40 +247,42 @@ subroutine PrepMetVarsForGOCART(metstate, tmpu, rhoa, hghte, lwi, ustar, pblh, h
    errMsg = ''
    thisloc = ' -> at PrepMetVarsForGOCART (in process/drydep/ccpr_DryDep_mod.F90)'
 
-   km  = MetState%NLEVS
-
-   if (.not. allocated(tmpu)) then
-      allocate(tmpu(1,1, km), stat=rc)
+   if (.not. allocated(GOCART_tmpu)) then
+      allocate(GOCART_tmpu(1,1, km), stat=rc)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Could not allocate tmpu'
          CALL CC_Error( errMsg, RC, thisLoc )
       endif
    endif
 
-   if (.not. allocated(rhoa)) then
-      allocate(rhoa(1,1,km), stat=rc)
+   if (.not. allocated(GOCART_RHOA)) then
+      allocate(GOCART_RHOA(1,1,km), stat=rc)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Could not allocate rhoa'
          CALL CC_Error( errMsg, RC, thisLoc )
       endif
    endif
 
-   if (.not. allocated(hghte)) then
-      allocate(hghte(1,1,km), stat=rc)
+   if (.not. allocated(GOCART_HGHTE)) then
+      allocate(GOCART_HGHTE(1,1,km), stat=rc)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Could not allocate hghte'
          CALL CC_Error( errMsg, RC, thisLoc )
       endif
    endif
 
-   tmpu = reshape(metstate%T, (/1, 1, km/))         ! temperature [K]
-   rhoa = reshape(metstate%AIRDEN, (/1, 1, km/)) ! air density [kg/m^3]
-   hghte = reshape(metstate%ZMID, (/1, 1, km/))    ! top of layer geopotential height [m]
-   lwi    = metstate%LWI       ! orography flag; Land, ocean, ice mask
-   ustar  = metstate%ustar     ! friction speed [m/sec]
-   pblh   = metstate%pblh      ! PBL height [m]
-   hflux = metstate%hflux     ! sfc. sens. heat flux [W m-2]
-   z0h    = metstate%z0h       ! rough height, sens. heat [m]
+   GOCART_tmpu = reshape(tmpu, (/1, 1, km/))         ! temperature [K]
+   GOCART_RHOA = reshape(AIRDEN, (/1, 1, km/)) ! air density [kg/m^3]
+   GOCART_HGHTE = reshape(hghte, (/1, 1, km/))    ! top of layer geopotential height [m]
+   GOCART_LWI    = LWI       ! orography flag; Land, ocean, ice mask
+   GOCART_USTAR  = ustar     ! friction speed [m/sec]
+   GOCART_PBLH   = pblh      ! PBL height [m]
+   GOCART_HFLUX = hflux     ! sfc. sens. heat flux [W m-2]
+   GOCART_Z0H    = z0h       ! rough height, sens. heat [m]
+   GOCART_U10 = u10
+   GOCART_V10 = v10
+   GOCART_FRACLAKE = fraclake
+   GOCART_GWETTOP = gwettop
 
 end subroutine PrepMetVarsForGOCART
 
