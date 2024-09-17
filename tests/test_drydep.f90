2fc7a5bfc7bdf1841f4cad8d4e1ffc5d4f44169d
lholland-noaa <165287123+lholland-noaa@users.noreply.github.com>
2024-09-17 10:46:11 -0400

    Update test_drydep.f90
    More edits

--- tests/test_drydep.f90
+++ tests/test_drydep.f90
@@ -73,7 +73,7 @@ program test_drydep
 
    do i = 1, MetState%NLEVS
       MetState%T(i)=273.15         ! K
-      MetState%MAIRDEN(i) = 1.2  ! kg/m3
+      MetState%MAIRDEN(i) = 1.2   ! kg/m3
       MetState%ZMID(i) = I*100   ! m
    end do
 
