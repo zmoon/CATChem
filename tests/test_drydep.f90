3b060dacb646de32f4637de45f4d47c8becbe091
lholland-noaa <165287123+lholland-noaa@users.noreply.github.com>
2024-09-18 16:53:27 -0400

    Update test_drydep.f90
    temporarily turning off print_info and assert for test3

--- tests/test_drydep.f90
+++ tests/test_drydep.f90
@@ -111,9 +111,9 @@ program test_drydep
    DryDepState%particleradius = 3.00
    DryDepState%particledensity = 2500.
    MetState%FRLAKE = 0
-   MetState%GWETTOP = 0.01_fp
-   MetState%U10m = 1.0_fp
-   MetState%V10m = 1.0_fp
+   MetState%GWETTOP = 0.01
+   MetState%U10m = 1.0
+   MetState%V10m = 1.0
 
 
    call cc_drydep_run(MetState, DiagState, DryDepState, ChemState, rc)
@@ -123,7 +123,7 @@ program test_drydep
       stop 1
    end if
 
-   call print_info(Config, DryDepState, MetState, title)
+  ! call print_info(Config, DryDepState, MetState, title)
   ! call assert(rae(DiagState%drydep_frequency(1), 0.0_fp), "Test 2 GOCART drydep Scheme (resuspension activated)")
 
 
