d903f18004531d2a4724dffe64b233f3fde84300
lholland-noaa <165287123+lholland-noaa@users.noreply.github.com>
2024-09-26 11:25:08 -0400

    Update CCPr_DryDep_Mod.F90

--- src/process/drydep/CCPr_DryDep_Mod.F90
+++ src/process/drydep/CCPr_DryDep_Mod.F90
@@ -242,7 +242,6 @@ CONTAINS
                   ! apply drydep velocities/freq to chem species
                   dqa = 0.
                   SpecConc = ChemState%chemSpecies(ChemState%DryDepIndex(i))%conc(1)
-                  print *, "SpecConc = ", SpecConc
                   dqa = MAX(0.0_fp, SpecConc * (1.-exp(-1*drydepf(1,1) * MetState%TSTEP)))
                   ChemState%chemSpecies(ChemState%DryDepIndex(i))%conc(1) = SpecConc - dqa
 
