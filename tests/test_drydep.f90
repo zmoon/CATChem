0efec5972224e650c3aea87ae1b642351ba2a1fe
lholland-noaa <165287123+lholland-noaa@users.noreply.github.com>
2024-10-01 13:15:34 -0400

    Update test_drydep.f90
    RAE is missing, removed references to it.

--- tests/test_drydep.f90
+++ tests/test_drydep.f90
@@ -134,7 +134,7 @@ program test_drydep
    end if
 
    call print_info(Config, DryDepState, MetState, title)
-   call assert((DiagState%drydep_frequency(1)), 0.0_fp, "Test 2 GOCART drydep Scheme (resuspension activated)")
+   call assert(DiagState%drydep_frequency(1) > 0.0_fp), "Test 2 GOCART drydep Scheme (resuspension activated)")
 
 
 
